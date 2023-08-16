0ab221795350792a3b24fe9540e47b90e1bfb0ec
coretech255 <ikehezechi@gmail.com>
2023-08-16 13:58:32 +0100

    Environment Variables Added

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -10,23 +10,38 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: don't run with debug turned on in production!
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, 0)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q!70!1ynd@e!9i&=k)@=ssaxx5g4cq3=)e&2%^tcj=pt(lfrj@'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Get the environment variable
+allowed_host_env = os.environ.get('ALLOWED_HOST')
 
-ALLOWED_HOSTS = []
+allowed_host = [host.strip() for host in allowed_host_env.split(',')]
+ALLOWED_HOSTS = allowed_host
+print(ALLOWED_HOSTS)
 
 
 # Application definition
